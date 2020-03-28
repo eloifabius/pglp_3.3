6ebb9e21f0849070fd071d7225ed0fdb268cd4aa
eloifabius <fabius1995>
2020-03-28 03:34:41 +0100

    création de la classe RobotStatique

---
+++ src/main/java/fr/uvsq/pglp/RobotStatique.java
@@ -0,0 +1,11 @@
+package fr.uvsq.pglp;
+
+public class RobotStatique {
+    private int position;
+    public RobotStatique(int position) {
+        this.position=position;
+    }
+    public void avance() {
+        throw new UnsupportedOperationException();
+    }
+}
